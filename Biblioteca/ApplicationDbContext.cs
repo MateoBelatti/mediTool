1ed422e657d42c3d0e4cf0c4edb25e4918c05943
MateoBelatti <mateobelatti7@gmail.com>
2026-07-15 03:28:07 -0300

    context db

--- Biblioteca/ApplicationDbContext.cs
+++ Biblioteca/ApplicationDbContext.cs
@@ -13,6 +13,8 @@ namespace Biblioteca.Repository
         public DbSet<Profesional> Profesionales { get; set; }
         public DbSet<Paciente> Pacientes { get; set; }
         public DbSet<PacienteProfesional> PacienteProfesionales { get; set; }
+        public DbSet<Informe> Informes { get; set; }
+        public DbSet<Reunion> Reuniones { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -41,6 +43,26 @@ namespace Biblioteca.Repository
                 .HasOne(pp => pp.Profesional)
                 .WithMany(p => p.PacienteProfesionales)
                 .HasForeignKey(pp => pp.ProfesionalId);
+
+            // Configure Informe relationships
+            modelBuilder.Entity<Informe>()
+                .HasOne(i => i.Paciente)
+                .WithMany(p => p.Informes)
+                .HasForeignKey(i => i.PacienteId)
+                .OnDelete(DeleteBehavior.SetNull); // Or NoAction, depending on requirements
+
+            modelBuilder.Entity<Informe>()
+                .HasOne(i => i.Profesional)
+                .WithMany(p => p.Informes)
+                .HasForeignKey(i => i.ProfesionalId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Configure Reunion relationships
+            modelBuilder.Entity<Reunion>()
+                .HasOne(r => r.Profesional)
+                .WithMany(p => p.Reuniones)
+                .HasForeignKey(r => r.ProfesionalId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
